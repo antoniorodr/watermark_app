53fd84804eb75d77093edc615bbfe1bf321b18ab
Antonio Rodriguez <antonioinorge@hotmail.com>
2025-01-10 13:36:43 +0100

    add watermark img and function, with a messagebox when done

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from tkinter import Tk, Button, filedialog, Label
+from tkinter.messagebox import showinfo
 from PIL import Image
+import os
 
 def gui():
     window = Tk()
     window.title("Watermarking APP")
     window.config(padx = 100, pady = 50, bg = "#9bdeac")
+    window.eval('tk::PlaceWindow . center')
 
     button = Button(text = "Upload", font = ("Courier", 20, "bold"), command = upload_file)
     button.grid(column = 0, row = 4)
@@ -16,7 +19,19 @@ def gui():
 
 def upload_file():
     file = filedialog.askopenfilename(title="Select a File", filetypes=[("PNG files", "*.png"), ("JPG files", "*.jpg")])
-    return file
+    return add_watermark(file)
+
+def add_watermark(file):
+    image = Image.open(file)
+    watermark = Image.open("watermark/default.png")
+    position = (10, 10)
+    image.paste(watermark, position, watermark)
+    downloads_path = os.path.join(os.path.expanduser("~"), "Downloads")
+    output_path = os.path.join(downloads_path, "output_image.png")
+    image.save(output_path)
+    image.close()
+    watermark.close()
+    showinfo("Done!", "Watermark added. Please check your Downloads folder.")
 
 
 if __name__ == "__main__":
