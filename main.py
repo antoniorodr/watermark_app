5487eb301e8a63116bb70d53fa88a9f944b682fa
Antonio Rodriguez <antonioinorge@hotmail.com>
2025-01-10 12:54:25 +0100

    gui and function for open the file

---
+++ main.py
@@ -0,0 +1,22 @@
+from tkinter import Tk, Button, filedialog, Label
+from PIL import Image
+
+def gui():
+    window = Tk()
+    window.title("Watermarking APP")
+    window.config(padx = 100, pady = 50, bg = "#9bdeac")
+
+    button = Button(text = "Upload", font = ("Courier", 20, "bold"), command = upload_file)
+    button.grid(column = 0, row = 4)
+
+    text = Label(text = "Choose your file", font = ("Courier", 20, "bold"), fg = "#9bdeac", bg = "#f7f5dd")
+    text.grid(column = 0, row = 6)
+
+    window.mainloop()
+
+def upload_file():
+    return filedialog.askopenfilename()
+
+
+if __name__ == "__main__":
+    gui()
